217ae15e8ee49d45eca3bca4db2a1fcebdfd4664
italo91206 <italoferreiradjk14@gmail.com>
2022-03-31 08:01:16 -0300

    added new pages

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,32 @@
 import Home from '@/pages/HomePage.vue'
 import Product from '@/pages/ProductPage.vue'
 import Search from '@/pages/SearchPage.vue'
+import MinhaContaPage from '@/pages/MinhaContaPage.vue'
+import EntrarPage from '@/pages/EntrarPage.vue'
 
 const routes = [
   {
     path: '/',
     component: Home,
     name: 'Página Home',
+    meta: {
+      title: 'WK3 Outlet',
+      layout: 'AppLayoutDefault'
+    }
+  },
+  {
+    path: '/minha-conta',
+    component: MinhaContaPage,
+    name: 'Minha conta',
+    meta: {
+      title: 'WK3 Outlet',
+      layout: 'AppLayoutDefault'
+    }
+  },
+  {
+    path: '/entrar',
+    component: EntrarPage,
+    name: 'Entre ou cadastre-se',
     meta: {
       title: 'WK3 Outlet',
       layout: 'AppLayoutDefault'
