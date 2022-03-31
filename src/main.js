01b1a0ce4c31b1574b807f012dc3d89903096585
italo91206 <italoferreiradjk14@gmail.com>
2022-03-31 08:01:09 -0300

    auto capture user token when routign

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
+import Vuex from 'vuex'
+import store from './store';
 import VueRouter from 'vue-router'
 import VueToast from 'vue-toast-notification';
+import jwt_decode from 'jwt-decode';
 import 'vue-toast-notification/dist/theme-default.css';
 import App from './App.vue'
 import routes from './routes.js'
@@ -9,14 +12,36 @@ Vue.config.productionTip = false
 
 Vue.use(VueRouter);
 Vue.use(VueToast);
+Vue.use(Vuex);
 
 const router = new VueRouter({
   mode: 'history',
   routes
 })
 
+router.beforeEach((to, from, next) => {
+  const user = localStorage.getItem('user');
+
+  if(user != null){
+    let user_decoded = jwt_decode(user);
+    let exp_date = new Date(user_decoded.exp * 1000);
+
+    // console.log("user_decoded", user_decoded)
+    // console.log("exp_date", exp_date)
+
+    if(exp_date > new Date()){
+      store.commit('perfil/setPerfil', user_decoded.usuario);
+    }
+    else
+      localStorage.removeItem('user')
+  }
+
+  next();
+})
+
 new Vue({
   router,
   VueToast,
+  store,
   render: h => h(App),
 }).$mount('#app')
