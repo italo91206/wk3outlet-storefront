1f643e5dc8543398c88f0d93362ec908ed7d9952
italo91206 <italoferreiradjk14@gmail.com>
2022-04-14 06:47:58 -0300

    new components added

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,21 +1,179 @@
 <template>
   <footer id="wk-footer">
-    <h2>Este é o footer</h2>
+    <section class="footer-section">
+      <div class="container flex">
+        <article class="footer-section--item">
+          <h4>Institucional</h4>
+          <ul class="footer-list">
+            <li><router-link to="/">Quem somos</router-link></li>
+            <li><router-link to="/">Entrega Expressa</router-link></li>
+          </ul>
+        </article>
+
+        <article class="footer-section--item">
+          <h4>Ajuda</h4>
+          <ul class="footer-list">
+            <li><router-link to="/">Política de privacidade</router-link></li>
+            <li><router-link to="/">Trocas e devoluções</router-link></li>
+            <li><router-link to="/">Dúvidas frequentes</router-link></li>
+          </ul>
+        </article>
+
+        <article class="footer-section--item">
+          <h4>Contato</h4>
+          <ul class="footer-list">
+            <li><router-link to="/">(44) 99727-0403</router-link></li>
+            <li><router-link to="/">kalleu2799@gmail.com</router-link></li>
+          </ul>
+        </article>
+
+        <article class="footer-section--item">
+          <h4>Redes sociais</h4>
+          <ul class="footer-list footer-list--social">
+            <li>
+              <router-link to="/"
+                ><span class="fab fa-facebook-f"></span
+              ></router-link>
+            </li>
+            <li>
+              <router-link to="/"
+                ><span class="fab fa-instagram"></span
+              ></router-link>
+            </li>
+          </ul>
+        </article>
+
+        <article class="footer-section--item">
+          <h4>Ajuda</h4>
+          <ul class="footer-list">
+            <li><router-link to="/">Política de privacidade</router-link></li>
+            <li><router-link to="/">Trocas e devoluções</router-link></li>
+            <li><router-link to="/">Dúvidas frequentes</router-link></li>
+          </ul>
+        </article>
+      </div>
+    </section>
+
+    <section class="footer-section">
+      <div class="container flex">
+        <article class="footer-section--item col-4">
+          <h4>Formas de pagamento</h4>
+          <img
+            src="https://i0.wp.com/urucuna.com/wp-content/uploads/2021/02/bandeiras-cartoes-credito.png?ssl=1"
+            alt=""
+          />
+        </article>
+
+        <article class="footer-section--item col-4">
+          <h4>Segurança</h4>
+
+          <div class="footer-section--wrapper flex space-between">
+            <img
+              src="https://increazy-folder.s3.amazonaws.com/5ee8c9130472895d074b1102/empresa-associada.png?v=1643115219"
+              alt=""
+              class="col-2"
+            />
+            <img
+              src="https://increazy-folder.s3.amazonaws.com/5ee8c9130472895d074b1102/e-bit.png?v=1643115219"
+              alt=""
+              class="col-2"
+            />
+            <img
+              src="https://increazy-folder.s3.amazonaws.com/5ee8c9130472895d074b1102/compra-segura.png?v=1643115219"
+              alt=""
+              class="col-2"
+            />
+          </div>
+        </article>
+
+        <article class="footer-section--item col-4">
+          <h4>WK3 Outlet</h4>
+          <p>
+            Com 14 anos de muita história para contar, hoje temos 12 lojas
+            espalhadas por todo o estado do Paraná. A nossa trajetória, desde o
+            início, só acontece com um único objetivo: levar o que há de melhor
+            para os nossos clientes e com o menor preço sempre.
+          </p>
+        </article>
+      </div>
+    </section>
+
+    <section class="footer-copyright">
+      <h5>
+        © WK3 Outlet 2022. Todos os direitos reservados. Conheça nossa Política
+        de Privacidade <br />
+        R. Ricardo Kauffman 365, Centro - CNPJ: CNPJ 40.088.323/0001-01 -
+        Avenida Brasil, 456, Centro, CEP 87340-000, Mamboré – PR
+      </h5>
+    </section>
   </footer>
 </template>
 
 <script>
 export default {
-  name: "FooterComponent"
+  name: "FooterComponent",
 };
 </script>
 
 <style lang="css">
-#wk-footer {
-  height: 100px;
-  border-top: solid 2px #bdb9b9;
-  display: flex;
-  justify-content: center;
-  align-items: center;
+.footer-section--item h4 {
+  text-align: left;
+}
+
+.footer-section {
+  background: #2c3e50;
+  color: #fff;
+  justify-content: space-around;
+  padding: 30px 0;
+  border-bottom: solid 1px #ffffff69;
+}
+
+.footer-section .container.flex {
+  justify-content: space-between;
+}
+
+.footer-list {
+  margin-block: unset;
+  padding-inline: unset;
+  margin-inline: unset;
+  text-align: left;
+  list-style-type: none;
+}
+
+.footer-list li {
+  margin-bottom: 15px;
+  color: #d5d5d5;
+}
+
+.footer-list li:hover {
+  text-decoration: underline;
+}
+
+.footer-list--social li {
+  display: inline-block;
+  margin-right: 35px;
+  font-size: 22px;
+}
+
+.footer-section--item p {
+  text-align: left;
+}
+
+.footer-section--item img {
+  width: 100%;
+  object-fit: contain;
+}
+
+.footer-copyright {
+  background: #16222e;
+  padding: 30px 10px;
+}
+
+.footer-copyright h5 {
+  margin: unset;
+  max-width: 570px;
+  color: white;
+  text-align: center;
+  margin: 0 auto;
 }
 </style>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,28 +1,43 @@
 <template>
   <header id="wk-header">
-    <div class="wk-container flex">
-      <div id="wk-header--image">
-        <img src="logotipo-extenso.png" alt="" />
-      </div>
+    <section id="pre-header">
+      <div class="wk-container flex">
+        <div class="pre-header--item">
+          <span class="fas fa-credit-card"></span>
+          <p>Pague em até 12x sem juros</p>
+        </div>
 
-      <div id="wk-header--menu">
-        <ul class="items-menu flex">
-          <li><router-link to="/">Home</router-link></li>
-        </ul>
+        <div class="pre-header--item">
+          <span class="fas fa-store"></span>
+          <p>Compre aqui e retire na loja</p>
+        </div>
+
+        <div class="pre-header--item">
+          <span class="fas fa-truck"></span>
+          <p>Entrega para todo o Brasil</p>
+        </div>
+      </div>
+    </section>
+
+    <div class="wk-container wk-container--principal flex">
+      <div id="wk-header--image">
+        <router-link to="/">
+          <img src="logo-extenso-invertido.png" alt="" />
+        </router-link>
       </div>
 
       <div id="wk-header--search">
         <form class="search-bar">
-          <input type="text" />
+          <input type="text" placeholder="Do que você precisa ?" />
           <span class="fas fa-search"></span>
         </form>
       </div>
 
       <div id="wk-header--options">
         <router-link to="/minha-conta">
-          <span class="fas fa-user"></span>
+          <span class="fa-user" :class="{'far': isLogado == false, 'fas': isLogado}"></span>
           <span v-if="isLogado">Olá, {{ getNomePerfil }}</span>
-          <span v-else>{{ getNomePerfil }}</span>
+          <span v-else>Olá visitante</span>
         </router-link>
 
         <span
@@ -30,6 +45,17 @@
           @click="carrinhoToggled = !carrinhoToggled"
         ></span>
       </div>
+    </div>
+
+    <div class="wk-container-menu flex">
+        <ul class="items-menu menu-container flex">
+          <li><router-link to="/">Camisetas</router-link></li>
+          <li><router-link to="/">Calças</router-link></li>
+          <li><router-link to="/">Tênis</router-link></li>
+          <li><router-link to="/">Acessórios</router-link></li>
+          <li><router-link to="/">Destaques</router-link></li>
+          <li><router-link to="/">Promoções</router-link></li>
+        </ul>
     </div>
 
     <Carrinho
@@ -40,12 +66,12 @@
 </template>
 
 <script>
-import Carrinho from '@/components/Carrinho'
+import Carrinho from "@/components/Carrinho";
 
 export default {
   name: "HeaderComponent",
   components: {
-    Carrinho
+    Carrinho,
   },
   data() {
     return {
@@ -53,21 +79,18 @@ export default {
     };
   },
   computed: {
-    getNomePerfil(){
-      let perfil = this.$store.getters['perfil/getPerfil']
-      if(perfil == null)
-        return "deslogado"
-      else
-        return perfil.nome;
+    getNomePerfil() {
+      let perfil = this.$store.getters["perfil/getPerfil"];
+      if (perfil == null) return "deslogado";
+      else return perfil.nome;
     },
-    isLogado(){
-      let perfil = this.$store.getters['perfil/getPerfil']
+    isLogado() {
+      let perfil = this.$store.getters["perfil/getPerfil"];
       // console.log("perfil", perfil )
-      return perfil.nome == null ? false : true
+      return perfil.nome == null ? false : true;
     },
   },
-  methods: {
-  },
+  methods: {},
 };
 </script>
 
@@ -78,18 +101,19 @@ export default {
 
 .search-bar .fas {
   position: absolute;
-  top: 6px;
+  top: 12px;
   right: 8px;
   cursor: pointer;
 }
 
 .search-bar input {
-  padding: 5px 30px 5px 5px;
+  padding: 10px 30px 10px 15px;
+  width: 310px;
 }
 
 #wk-header {
-  height: 100px;
-  background: #ffaa2a;
+  /* height: 100px; */
+  border-bottom: solid 2px #2c3e50;
 }
 .items-menu {
   width: 100%;
@@ -98,7 +122,7 @@ export default {
   max-width: 1240px;
   margin: 0 auto;
   width: 100%;
-  padding: 0px !important;
+  padding: 0px;
 }
 
 .wk-container.flex {
@@ -108,22 +132,61 @@ export default {
 
 .items-menu li {
   display: block;
-  margin-right: 15px;
-  color: #fff;
+  color: #ffaa2a;
   text-transform: uppercase;
   font-family: "roboto";
+  font-weight: bold;
 }
 
 .items-menu li:last-child {
   margin: unset;
 }
 
-#wk-header--options .fas {
+#wk-header--options .fas,
+#wk-header--options .far {
   font-size: 24px;
   margin: 0px 6px;
 }
 
-#wk-header--options .fas:hover {
+#wk-header--options .fas:hover,
+#wk-header--options .far:hover {
   cursor: pointer;
+}
+
+.wk-container-menu {
+  border-top: solid 1px #80808024;
+  justify-content: space-around;
+}
+
+.wk-container--principal {
+  padding: 15px 0;
+}
+
+.wk-container-menu {
+  border-top: solid 1px #8080805c;
+  justify-content: space-around;
+}
+
+.wk-container--principal {
+  padding: 15px 0;
+}
+
+#pre-header {
+  background: #2c3e50;
+  color: #fff;
+}
+
+.pre-header--item {
+  display: flex;
+  align-items: center;
+}
+
+.pre-header--item .fas {
+  margin-right: 10px;
+}
+
+.menu-container{
+  max-width: 960px;
+  justify-content: space-between;
 }
 </style>
