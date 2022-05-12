a67ab1f1726b865822ed5971b352151b7a9c50eb
italo91206 <italoferreiradjk14@gmail.com>
2022-05-12 05:31:22 -0300

    added swiper

--- src/components/ProductSearch.vue
+++ src/components/ProductSearch.vue
@@ -1,13 +1,17 @@
 <template>
   <section class="product-listing">
-    <router-link
-      v-for="produto in produtos"
-      :key="produto.id"
-      class="product-router-link"
-      :to="`/p/${produto.sku}`"
-    >
-      <ProductCard :produto="produto"/>
-    </router-link>
+    <div class="swiper">
+      <div class="swiper-wrapper">
+        <router-link
+          v-for="produto in produtos"
+          :key="produto.id"
+          class="product-router-link swiper-slide"
+          :to="`/p/${produto.sku}`"
+        >
+          <ProductCard :produto="produto" />
+        </router-link>
+      </div>
+    </div>
   </section>
 </template>
 
@@ -23,28 +27,64 @@ export default {
   data() {
     return {
       produtos: [],
-      swiper: {},
+      loading: true,
+      swiper: null,
     };
   },
   methods: {
     async listarProdutos() {
-      const response = await service.listarProdutos();
-      if (response.data.success) this.produtos = response.data.data;
+      await service
+        .listarProdutos()
+        .then((response) => {
+          if (response.data.success) this.produtos = response.data.data;
+          else this.$toast.error(response.data.message);
+        })
+        .catch((error) => {
+          console.log("ProductSearch component: something bad happened");
+          console.log(error);
+        })
+        .finally(() => {
+          this.loading = false;
+        });
     },
   },
   mounted() {
+    let script = document.createElement("script");
+    script.setAttribute(
+      "src",
+      "https://cdnjs.cloudflare.com/ajax/libs/Swiper/6.1.0/swiper-bundle.min.js"
+    );
+    script.setAttribute("crossorigin", "anonymous");
+    script.setAttribute("referrerpolicy", "no-referrer");
+    script.setAttribute(
+      "integrity",
+      "sha512-uz9KhDW9ZdiJU79RDPNuHE4Z9aUOYTVargiMzYbe8Z3j5vBHxBMmlvGw1Xa09CmV6tCUOhGazG4pTWsuDJd1xw=="
+    );
+    script.onload = () => {
+      // eslint-disable-next-line
+      new Swiper(".swiper", {
+        slidesPerView: 'auto',
+        spaceBetween: 10,
+      });
+    };
+    document.body.appendChild(script);
     this.listarProdutos();
   },
+  watch: {},
 };
 </script>
 
 <style lang="css">
-.product-router-link{
+.swiper-slide.product-router-link {
   display: block;
+  width: unset;
 }
-.product-listing {
-    display: flex;
-    flex-wrap: wrap;
-    justify-content: space-between;
+.swiper {
+  overflow: hidden;
 }
+/* .product-listing {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+} */
 </style>
